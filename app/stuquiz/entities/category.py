c666452d87cb9eaa1882dc827ec978cb0724d1eb
Simone Nicol <en0mia.dev@gmail.com>
2023-05-30 12:52:34 +0200

    :card_file_box: Add Category table, Category Entity and relations with Course. Close #13

---
+++ app/stuquiz/entities/category.py
@@ -0,0 +1,14 @@
+# @author Simone Nicol <en0mia.dev@gmail.com>
+# @created 30/05/23
+from dataclasses import dataclass
+from app.stuquiz.entities.entity import Entity
+
+
+@dataclass
+class Category(Entity):
+    """
+    This class represents a Category object.
+    """
+    id: int
+    uuid: str
+    name: str
