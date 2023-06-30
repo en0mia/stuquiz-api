dcc069bb0482d24bb9d2c2d0ddf37adf0b3086ab
Simone Nicol <en0mia.dev@gmail.com>
2023-06-30 15:35:19 +0200

    :art: Move uuid field to id for the entities.

--- db/init.sql
+++ db/init.sql
@@ -5,27 +5,24 @@ USE stuquiz;
 -- Tables definitions
 
 CREATE TABLE IF NOT EXISTS university (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  uuid UUID UNIQUE NOT NULL DEFAULT UUID(),
+  id UUID PRIMARY KEY DEFAULT UUID(),
   name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS category (
-    id INTEGER PRIMARY KEY,
-    uuid UUID UNIQUE NOT NULL DEFAULT UUID(),
+    id UUID PRIMARY KEY DEFAULT UUID(),
     name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS course (
-  id INTEGER PRIMARY KEY,
-  uuid UUID UNIQUE NOT NULL DEFAULT UUID(),
+  id UUID PRIMARY KEY DEFAULT UUID(),
   university_id UUID NOT NULL,
   name TEXT NOT NULL,
   description TEXT NOT NULL,
   professor TEXT NOT NULL,
   code TEXT NOT NULL,
   CONSTRAINT `fk_course_university`
-      FOREIGN KEY (university_id) REFERENCES university(uuid)
+      FOREIGN KEY (university_id) REFERENCES university(id)
           ON DELETE CASCADE
           ON UPDATE CASCADE
 );
@@ -35,38 +32,36 @@ CREATE TABLE IF NOT EXISTS course_category (
     category_id UUID NOT NULL,
     PRIMARY KEY (course_id, category_id),
     CONSTRAINT `fk_course_category_course`
-      FOREIGN KEY (course_id) REFERENCES course(uuid)
+      FOREIGN KEY (course_id) REFERENCES course(id)
           ON DELETE CASCADE
           ON UPDATE CASCADE,
     CONSTRAINT `fk_course_category_category`
-      FOREIGN KEY (category_id) REFERENCES category(uuid)
+      FOREIGN KEY (category_id) REFERENCES category(id)
           ON DELETE CASCADE
           ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS question (
-  id INTEGER PRIMARY KEY,
-  uuid UUID UNIQUE NOT NULL DEFAULT UUID(),
+  id UUID PRIMARY KEY DEFAULT UUID(),
   course_id UUID NOT NULL,
   question TEXT NOT NULL,
   creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   rating INT NOT NULL DEFAULT 0,
   CONSTRAINT `fk_question_course`
-      FOREIGN KEY (course_id) REFERENCES course(uuid)
+      FOREIGN KEY (course_id) REFERENCES course(id)
           ON DELETE CASCADE
           ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS answer (
-  id INTEGER PRIMARY KEY,
-  uuid UUID UNIQUE NOT NULL DEFAULT UUID(),
+  id UUID PRIMARY KEY DEFAULT UUID(),
   question_id UUID NOT NULL,
   answer TEXT NOT NULL,
   creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   correct INTEGER NOT NULL DEFAULT 0,
   points FLOAT NOT NULL DEFAULT 1.0,
   CONSTRAINT `fk_answer_question`
-      FOREIGN KEY (question_id) REFERENCES question(uuid)
+      FOREIGN KEY (question_id) REFERENCES question(id)
           ON DELETE CASCADE
           ON UPDATE CASCADE
 );
