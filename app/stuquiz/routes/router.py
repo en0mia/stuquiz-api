a4fb62caaf63a8d2994914340e390e18a422b5ff
Simone Nicol <en0mia.dev@gmail.com>
2023-07-02 22:34:44 +0200

    :art: Define the structure for the next features.

---
+++ app/stuquiz/routes/router.py
@@ -0,0 +1,9 @@
+# @author Simone Nicol <en0mia.dev@gmail.com>
+# @created 02/07/23
+
+def register_routes(app) -> None:
+    """Registers the blueprints (aka the routes) of the app.
+    :param app: the app in which we want to register the blueprints.
+    :return: void
+    """
+    pass
