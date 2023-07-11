2af9c5f36c97b4e1543112c71207dbb8af4aa5c6
Lorenzo Varese <vareselorenzo@gmail.com>
2023-07-11 23:13:57 +0200

    Query parameters converted to tuples

--- app/stuquiz/repositories/course_repository.py
+++ app/stuquiz/repositories/course_repository.py
@@ -14,7 +14,7 @@ class CourseRepository(AbstractRepository):
 
     def delete_course(self, course: Course) -> bool:
         query = "DELETE FROM course WHERE course_id = %s"
-        return self.delete(query, course.id)
+        return self.delete(query, (course.id, ))
 
     def update_course(self, course: Course) -> bool:
         query = "UPDATE course SET name = %s, description = %s, professor = %s, " \
@@ -22,7 +22,7 @@ class CourseRepository(AbstractRepository):
         return self.update(query, (course.name, course.description, course.professor,
                            course.code, course.university_id, course.id))
 
-    def select_course_by_id(self, course_id: int) -> Course:
+    def select_course_by_id(self, course_id: str) -> Course:
         query = "SELECT * FROM course WHERE id = %s"
-        result = self.select(query, course_id)
+        result = self.select(query, (course_id, ))
         return Course(*result[0]) if result and len(result) > 0 else None
