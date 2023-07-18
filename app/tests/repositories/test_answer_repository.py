b29ba4703cb9d1467ec27f4ca7e3767049ea1032
Simone Nicol <en0mia.dev@gmail.com>
2023-07-18 12:17:38 +0200

    :art: Standardise tests

--- app/tests/repositories/test_answer_repository.py
+++ app/tests/repositories/test_answer_repository.py
@@ -11,7 +11,7 @@ from app.stuquiz.repositories.answer_repository import AnswerRepository
 
 
 class TestAnswerRepository(unittest.TestCase):
-    test_answer = None
+    TEST_ANSWER = Answer(str(uuid.uuid4()), str(uuid.uuid4()), "Test answer", datetime.now(), True, 10.0)
 
     def setUp(self) -> None:
         self.cursor = MagicMock()
@@ -22,61 +22,47 @@ class TestAnswerRepository(unittest.TestCase):
         self.db_provider.get_db.return_value = self.db
         self.repository = AnswerRepository(self.db_provider)
 
-        self.test_answer = Answer(
-            id=str(uuid.uuid4()),
-            question_id=str(uuid.uuid4()),
-            answer="Test answer",
-            creation_date=datetime.now(),
-            correct=True,
-            points=10.0
-        )
-
     def tearDown(self) -> None:
         self.cursor = None
         self.db = None
         self.db_provider = None
         self.repository = None
-        self.test_answer = None
 
-    def test_create_answer(self):
+    def testCreateAnswer_ReturnTrue_WhenAnswerCreated(self):
         # Arrange
         expected_query = 'INSERT INTO answer(question_id, answer, creation_date, correct, points) ' \
                          'VALUES (%s, %s, %s, %s, %s);'
-        expected_args = (self.test_answer.question_id, self.test_answer.answer, self.test_answer.creation_date,
-                         self.test_answer.correct, self.test_answer.points)
+        expected_args = (self.TEST_ANSWER.question_id, self.TEST_ANSWER.answer, self.TEST_ANSWER.creation_date,
+                         self.TEST_ANSWER.correct, self.TEST_ANSWER.points)
 
         # Act
-        result = self.repository.create_answer(self.test_answer)
+        result = self.repository.create_answer(self.TEST_ANSWER)
 
         # Assert
         self.cursor.execute.assert_called_once_with(expected_query, expected_args)
-
         self.db.commit.assert_called_once()
-
         self.assertTrue(result)
 
-    def test_delete_answer(self):
+    def testDeleteAnswer_ReturnTrue_WhenAnswerDeleted(self):
         # Arrange
         expected_query = 'DELETE FROM answer WHERE id = %s'
-        expected_arg = (self.test_answer.id, )
+        expected_arg = (self.TEST_ANSWER.id, )
 
         # Act
-        result = self.repository.delete_answer(self.test_answer)
+        result = self.repository.delete_answer(self.TEST_ANSWER)
 
         # Assert
         self.cursor.execute.assert_called_once_with(expected_query, expected_arg)
-
         self.db.commit.assert_called_once()
-
         self.assertTrue(result)
 
-    def test_update_answer_text(self):
+    def testUpdateAnswer_ReturnTrue_WhenAnswerUpdated(self):
         # Arrange
         expected_query = 'UPDATE answer SET answer = %s WHERE id = %s'
-        expected_args = (self.test_answer.answer, self.test_answer.id)
+        expected_args = (self.TEST_ANSWER.answer, self.TEST_ANSWER.id)
 
         # Act
-        result = self.repository.update_answer_text(self.test_answer)
+        result = self.repository.update_answer_text(self.TEST_ANSWER)
 
         # Assert
         self.cursor.execute.assert_called_once_with(expected_query, expected_args)
@@ -85,22 +71,22 @@ class TestAnswerRepository(unittest.TestCase):
 
         self.assertTrue(result)
 
-    def test_select_answer_by_id_when_answer_exists(self):
+    def testSelectAnswerById_ReturnAnswer_WhenAnswerExist(self):
         # Arrange
-        expected_result = Answer(self.test_answer.id, self.test_answer.question_id,
-                                 self.test_answer.answer, self.test_answer.creation_date,
-                                 self.test_answer.correct, self.test_answer.points)
-        self.cursor.fetchall.return_value = [(self.test_answer.id, self.test_answer.question_id,
-                                              self.test_answer.answer, self.test_answer.creation_date,
-                                              self.test_answer.correct, self.test_answer.points)]
+        expected_result = Answer(self.TEST_ANSWER.id, self.TEST_ANSWER.question_id,
+                                 self.TEST_ANSWER.answer, self.TEST_ANSWER.creation_date,
+                                 self.TEST_ANSWER.correct, self.TEST_ANSWER.points)
+        self.cursor.fetchall.return_value = [(self.TEST_ANSWER.id, self.TEST_ANSWER.question_id,
+                                              self.TEST_ANSWER.answer, self.TEST_ANSWER.creation_date,
+                                              self.TEST_ANSWER.correct, self.TEST_ANSWER.points)]
 
         # Act
-        result = self.repository.select_answer_by_id(self.test_answer.id)
+        result = self.repository.select_answer_by_id(self.TEST_ANSWER.id)
 
         # Assert
         self.assertEqual(expected_result, result)
 
-    def test_select_answer_by_id_when_answer_doesnt_exist(self):
+    def testSelectAnswerById_ReturnNone_WhenAnswerDontExist(self):
         # Arrange
         self.cursor.fetchall.return_value = None
 
